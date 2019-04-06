ff924891f2bf3668926071b0bc8e8b6005895582
Gabriel Tofvesson <gabto095@student.liu.se>
2019-04-06 23:16:13 +0200

    Add program to random-generate hex values

---
+++ rand_gen.py
@@ -0,0 +1,9 @@
+import random
+
+def getRandomHex():
+    return hex(random.randint(0, 0x10000) | (1 << 30))[-4:].upper()
+
+if __name__ == "__main__":
+    print("@0xE0")
+    for i in range(0, 32):
+        print(getRandomHex())
