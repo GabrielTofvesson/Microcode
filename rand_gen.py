00379b472b5348aba1a974045255844fcded35ba
Gabriel Tofvesson <gabto095@student.liu.se>
2019-04-10 00:04:33 +0200

    Add explicit memory type declaration to random generator

--- rand_gen.py
+++ rand_gen.py
@@ -4,6 +4,6 @@ def getRandomHex():
     return hex(random.randint(0, 0x10000) | (1 << 30))[-4:].upper()
 
 if __name__ == "__main__":
-    print("@0xE0")
+    print("@p\n@0xE0")
     for i in range(0, 32):
         print(getRandomHex())
